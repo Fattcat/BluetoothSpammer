7c4160a4bf3449b332f2f3a71cc0e83499f694ca
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-12-24 09:57:45 +0000

    Update Scan.py

--- Scan.py
+++ Scan.py
@@ -3,6 +3,11 @@ import time
 import subprocess
 import platform
 
+CheckSUDO = os.getuid()
+if CHeckSUDO != 0:
+  print("Cant start without sudo permission")
+  print("Please run script like this : sudo python3 BlueScan.py")
+
 CheckOS = platform.platform()
 if CheckOS == "Windows":
   print("cant start on windows :/")
